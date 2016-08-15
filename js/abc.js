81837ac3978edb9e0c75ca76b90efa487e3c81b7
ljj <569728819@qq.com>
2016-08-15 10:27:53 +0800

    上传文件

---
+++ js/abc.js
@@ -0,0 +1,62 @@
+var screenObj=document.getElementById('screen');
+function inputKey(num){
+	if(clearBool){
+		screenObj.value='';
+		clearBool=false;
+	}
+	if(screenObj.value=='0' && num!='.'){
+		screenObj.value=num;
+	}else{
+		if((screenObj.value.indexOf(".")==-1 && num==".") || num!='.'){
+			screenObj.value+=num;
+		}
+	}
+}
+function clearFun(){
+	screenObj.value='';
+}
+function sqr(){
+	screenObj.value=Math.sqrt(screenObj.value);
+}
+function del(){
+	screenObj.value=screenObj.value.substring(0,screenObj.value.length-1);
+}
+var beforNum,afterNum,signGlobal,clearBool=false;
+function operatingFun(sign){
+	
+	if(signGlobal!=null){
+		result();
+		beforNum=resultNum;
+		signGlobal=sign;
+		afterNum=null;
+	}else{
+		beforNum=screenObj.value;
+		signGlobal=sign;
+		clearBool=true;
+	}
+}
+var resultNum='';
+function result(){
+	afterNum=screenObj.value;
+	switch (signGlobal){
+		case '+':
+			screenObj.value=Number(beforNum)+Number(afterNum);
+			break;
+		case '-':
+			screenObj.value=Number(beforNum)-Number(afterNum);
+			break;
+		case '*':
+			screenObj.value=Number(beforNum)*Number(afterNum);
+			break;
+		case '/':
+			screenObj.value=Number(beforNum)/Number(afterNum);
+			break;
+		case '%':
+			screenObj.value=Number(beforNum)%Number(afterNum);
+			break;
+		default:
+			break;
+	}
+	clearBool=true;
+	resultNum=screenObj.value;
+}
